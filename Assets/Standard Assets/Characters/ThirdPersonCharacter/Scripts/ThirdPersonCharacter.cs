bc4b14914aad38ec1e752c6e350b4402263cf639
Akita <t61789@163.com>
2020-06-27 16:17:05 +0800

    玩家控制第三人称场景

--- Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonCharacter.cs
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonCharacter.cs
@@ -29,15 +29,17 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 		CapsuleCollider m_Capsule;
 		bool m_Crouching;
 
-
 		void Start()
 		{
+            // 初始化和缓存需要的数据
+
 			m_Animator = GetComponent<Animator>();
 			m_Rigidbody = GetComponent<Rigidbody>();
 			m_Capsule = GetComponent<CapsuleCollider>();
 			m_CapsuleHeight = m_Capsule.height;
 			m_CapsuleCenter = m_Capsule.center;
 
+			// 不允许旋转
 			m_Rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
 			m_OrigGroundCheckDistance = m_GroundCheckDistance;
 		}
@@ -45,31 +47,32 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 
 		public void Move(Vector3 move, bool crouch, bool jump)
 		{
-
+			// 把世界坐标下的输入向量转换成本地坐标下的旋转量和向前量
 			// convert the world relative moveInput vector into a local-relative
 			// turn amount and forward amount required to head in the desired
 			// direction.
-			if (move.magnitude > 1f) move.Normalize();
-			move = transform.InverseTransformDirection(move);
-			CheckGroundStatus();
-			move = Vector3.ProjectOnPlane(move, m_GroundNormal);
-			m_TurnAmount = Mathf.Atan2(move.x, move.z);
-			m_ForwardAmount = move.z;
+			if (move.magnitude > 1f) move.Normalize();	// 归一化
+			move = transform.InverseTransformDirection(move);	// 世界坐标转本地
+			CheckGroundStatus();	// 检查脚下
+			move = Vector3.ProjectOnPlane(move, m_GroundNormal);	// 把移动向量投影到地面上
+			m_TurnAmount = Mathf.Atan2(move.x, move.z);	// 绕y轴旋转量
+			m_ForwardAmount = move.z;	// 前进量
 
-			ApplyExtraTurnRotation();
+			ApplyExtraTurnRotation();	// 转向
 
+			// 速度的处理在滞空状态和在地面上是不同的
 			// control and velocity handling is different when grounded and airborne:
 			if (m_IsGrounded)
 			{
-				HandleGroundedMovement(crouch, jump);
+				HandleGroundedMovement(crouch, jump);	// 地面模式
 			}
 			else
 			{
-				HandleAirborneMovement();
+				HandleAirborneMovement();	// 滞空模式
 			}
 
-			ScaleCapsuleForCrouching(crouch);
-			PreventStandingInLowHeadroom();
+			ScaleCapsuleForCrouching(crouch);	// 蹲伏处理
+			PreventStandingInLowHeadroom();	// 避免在狭小空间站立
 
 			// send input and other state parameters to the animator
 			UpdateAnimator(move);
@@ -78,8 +81,13 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 
 		void ScaleCapsuleForCrouching(bool crouch)
 		{
-			if (m_IsGrounded && crouch)
+			// 蹲伏时缩放胶囊碰撞盒
+
+            if (m_IsGrounded && crouch)
 			{
+				// 在地面上而且主动蹲伏
+				// 不在蹲伏的话 碰撞盒高度和位置/2f
+
 				if (m_Crouching) return;
 				m_Capsule.height = m_Capsule.height / 2f;
 				m_Capsule.center = m_Capsule.center / 2f;
@@ -87,6 +95,8 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 			}
 			else
 			{
+				// 在狭小空间内只能蹲伏，否则恢复
+
 				Ray crouchRay = new Ray(m_Rigidbody.position + Vector3.up * m_Capsule.radius * k_Half, Vector3.up);
 				float crouchRayLength = m_CapsuleHeight - m_Capsule.radius * k_Half;
 				if (Physics.SphereCast(crouchRay, m_Capsule.radius * k_Half, crouchRayLength, Physics.AllLayers, QueryTriggerInteraction.Ignore))
@@ -102,6 +112,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 
 		void PreventStandingInLowHeadroom()
 		{
+			// 阻止在仅允许蹲伏的区域内站立
 			// prevent standing up in crouch-only zones
 			if (!m_Crouching)
 			{
@@ -117,6 +128,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 
 		void UpdateAnimator(Vector3 move)
 		{
+			// 更新动画状态机的参数
 			// update the animator parameters
 			m_Animator.SetFloat("Forward", m_ForwardAmount, 0.1f, Time.deltaTime);
 			m_Animator.SetFloat("Turn", m_TurnAmount, 0.1f, Time.deltaTime);
@@ -127,7 +139,10 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 				m_Animator.SetFloat("Jump", m_Rigidbody.velocity.y);
 			}
 
-			// calculate which leg is behind, so as to leave that leg trailing in the jump animation
+			// 计算哪条腿在后面，就可以让那条腿在跳跃动画播放的时候留在后面
+			// （这些代码依赖于我们动画具体的跑步循环偏移）
+			// 并且假设一条对在归一化的时间片[0,0.5]内经过经过另一条腿
+            // calculate which leg is behind, so as to leave that leg trailing in the jump animation
 			// (This code is reliant on the specific run cycle offset in our animations,
 			// and assumes one leg passes the other at the normalized clip times of 0.0 and 0.5)
 			float runCycle =
@@ -139,6 +154,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 				m_Animator.SetFloat("JumpLeg", jumpLeg);
 			}
 
+			// 这里可以调整行走和跑步的速度
 			// the anim speed multiplier allows the overall speed of walking/running to be tweaked in the inspector,
 			// which affects the movement speed because of the root motion.
 			if (m_IsGrounded && move.magnitude > 0)
@@ -147,6 +163,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 			}
 			else
 			{
+				// 空中不可调整
 				// don't use that while airborne
 				m_Animator.speed = 1;
 			}
@@ -155,6 +172,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 
 		void HandleAirborneMovement()
 		{
+			// 给予额外的重力
 			// apply extra gravity from multiplier:
 			Vector3 extraGravityForce = (Physics.gravity * m_GravityMultiplier) - Physics.gravity;
 			m_Rigidbody.AddForce(extraGravityForce);
@@ -165,9 +183,13 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 
 		void HandleGroundedMovement(bool crouch, bool jump)
 		{
+			// 在地面上的移动
+
+			// 检查跳跃的条件
 			// check whether conditions are right to allow a jump:
 			if (jump && !crouch && m_Animator.GetCurrentAnimatorStateInfo(0).IsName("Grounded"))
 			{
+				// 跳跃，速度的y轴分量设为m_JumpPower
 				// jump!
 				m_Rigidbody.velocity = new Vector3(m_Rigidbody.velocity.x, m_JumpPower, m_Rigidbody.velocity.z);
 				m_IsGrounded = false;
@@ -178,6 +200,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 
 		void ApplyExtraTurnRotation()
 		{
+			// 帮助角色转得更快，角度越大越快
 			// help the character turn faster (this is in addition to root rotation in the animation)
 			float turnSpeed = Mathf.Lerp(m_StationaryTurnSpeed, m_MovingTurnSpeed, m_ForwardAmount);
 			transform.Rotate(0, m_TurnAmount * turnSpeed * Time.deltaTime, 0);
@@ -201,25 +224,40 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 
 		void CheckGroundStatus()
 		{
+			// 检测脚下地面的函数，用于 浮空/跳跃 等状态的实现
+
 			RaycastHit hitInfo;
 #if UNITY_EDITOR
+			// 画出检测线
 			// helper to visualise the ground check ray in the scene view
 			Debug.DrawLine(transform.position + (Vector3.up * 0.1f), transform.position + (Vector3.up * 0.1f) + (Vector3.down * m_GroundCheckDistance));
 #endif
+			// 0.1f是个很小的向角色内部的偏移，运行一下就可以在editor里观察到
 			// 0.1f is a small offset to start the ray from inside the character
 			// it is also good to note that the transform position in the sample assets is at the base of the character
+			
+            // 从偏移后的坐标向下发射射线
 			if (Physics.Raycast(transform.position + (Vector3.up * 0.1f), Vector3.down, out hitInfo, m_GroundCheckDistance))
 			{
+				// 脚下是地面
+
 				m_GroundNormal = hitInfo.normal;
 				m_IsGrounded = true;
 				m_Animator.applyRootMotion = true;
 			}
 			else
 			{
+				// 不是地面
+
 				m_IsGrounded = false;
 				m_GroundNormal = Vector3.up;
 				m_Animator.applyRootMotion = false;
 			}
 		}
+
+        private void OnGUI()
+        {
+			GUI.Label(new Rect(0,0,100,50),m_TurnAmount.ToString() );
+        }
 	}
 }
--- Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
@@ -7,6 +7,8 @@ namespace UnityStandardAssets.Characters.ThirdPerson
     [RequireComponent(typeof (ThirdPersonCharacter))]
     public class ThirdPersonUserControl : MonoBehaviour
     {
+        // 这个类读取用户的输入并处理，将数据传给Move方法
+
         private ThirdPersonCharacter m_Character; // A reference to the ThirdPersonCharacter on the object
         private Transform m_Cam;                  // A reference to the main camera in the scenes transform
         private Vector3 m_CamForward;             // The current forward direction of the camera
@@ -16,6 +18,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         
         private void Start()
         {
+            // 需要使用到摄像机
             // get the transform of the main camera
             if (Camera.main != null)
             {
@@ -25,9 +28,11 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             {
                 Debug.LogWarning(
                     "Warning: no main camera found. Third person character needs a Camera tagged \"MainCamera\", for camera-relative controls.", gameObject);
+                // 这样的话我们就会使用self-relative控制，可能不是用户想要的
                 // we use self-relative controls in this case, which probably isn't what the user wants, but hey, we warned them!
             }
 
+            // 需要与ThirdPersonCharacter协同工作
             // get the third person character ( this should never be null due to require component )
             m_Character = GetComponent<ThirdPersonCharacter>();
         }
@@ -45,28 +50,33 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         // Fixed update is called in sync with physics
         private void FixedUpdate()
         {
+            // 读取输入
             // read inputs
             float h = CrossPlatformInputManager.GetAxis("Horizontal");
             float v = CrossPlatformInputManager.GetAxis("Vertical");
-            bool crouch = Input.GetKey(KeyCode.C);
+            bool crouch = Input.GetKey(KeyCode.C);  // 这里把键位写死了，为什么和上面一样？
 
+            // 计算角色的前进方向
             // calculate move direction to pass to character
             if (m_Cam != null)
             {
+                // 计算依赖于摄像机的方向来移动
                 // calculate camera relative direction to move:
                 m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
                 m_Move = v*m_CamForward + h*m_Cam.right;
             }
             else
             {
+                // 没有摄像机的话就使用基于世界坐标的方向
                 // we use world-relative directions in the case of no main camera
                 m_Move = v*Vector3.forward + h*Vector3.right;
             }
 #if !MOBILE_INPUT
+            // 左shift慢走
 			// walk speed multiplier
 	        if (Input.GetKey(KeyCode.LeftShift)) m_Move *= 0.5f;
 #endif
-
+            // 把所有数据传给角色控制器
             // pass all parameters to the character control script
             m_Character.Move(m_Move, crouch, m_Jump);
             m_Jump = false;
